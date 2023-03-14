2ad21fc3be12e4bf5b7eb39ba1a75ef532bb20ec
Jaime Uribe <jaime.uribe@ceiba.com.co>
2023-03-14 17:27:40 -0500

    Created project and first unit test

---
+++ ADNParqueadero/ADNParqueadero/ADNParqueaderoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADNParqueaderoApp.swift
+//  ADNParqueadero
+//
+//  Created by Jaime Alexander Uribe Uribe - Ceiba Software on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ADNParqueaderoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADNParqueadero/ADNParqueadero/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ADNParqueadero
+//
+//  Created by Jaime Alexander Uribe Uribe - Ceiba Software on 14/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
