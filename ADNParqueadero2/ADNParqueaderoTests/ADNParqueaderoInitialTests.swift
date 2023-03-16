85e4ac0c75ffce3bbb46d50ec6cefa9da910823b
Jaime Uribe <jaime.uribe@ceiba.com.co>
2023-03-16 08:18:40 -0500

    Updated version cocapods for jenkin

---
+++ ADNParqueadero2/ADNParqueaderoTests/ADNParqueaderoInitialTests.swift
@@ -0,0 +1,26 @@
+//
+//  ADNParqueaderoInitialTests.swift
+//
+//  Created by Jaime Alexander Uribe Uribe - Ceiba Software on 14/03/23.
+//
+
+import XCTest
+@testable import ADNParqueadero
+
+final class ADNParqueaderoInitialTests: XCTestCase {
+    var isTestInitial: Bool!
+
+    override func setUpWithError() throws {
+        isTestInitial = true
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        isTestInitial = nil
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        XCTAssertTrue(isTestInitial)
+    }
+}
